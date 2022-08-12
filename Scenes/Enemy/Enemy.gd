f5d78de373d3aee540f6156c0f55f6a0b01eda8a
10rdP4 <p4hdez@gmail.com>
2022-08-12 17:38:33 +0200

    shaders

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -7,6 +7,12 @@ var speed
 
 func take_damage(damage: int) -> void:
 	health = health - damage
+	$Sprite.material.set_shader_param("flash_modifier", 0.4)
+	$FlashTimer.start()
 
 func get_health() -> int:
 	return health
+
+func _on_FlashTimer_timeout() -> void:
+	$Sprite.material.set_shader_param("flash_modifier", 0)
+	pass # Replace with function body.
